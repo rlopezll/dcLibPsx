847b23b76e2166966f1731291fa2e12855864452
Quim Romo <quimromo@gmail.com>
2022-12-18 02:08:06 +0100

    WIP in simple memory system

---
+++ dcMemory.c
@@ -0,0 +1,67 @@
+
+
+#include "dcMemory.h"
+#include <malloc.h>
+
+unsigned long __ramsize = 0x00200000; // 2MB RAM
+unsigned long __stacksize = 0x00004000; // 16k stack
+
+void dcMemory_Init()
+{
+    #ifdef __GNUC__
+    // The GCC compiler we are using does not provide sectend() mips function, instead it provides those globals:
+    extern unsigned char __heap_start[];
+    extern unsigned char __stack_start[]; // Stack start from the end of ram and grows backwards   
+    
+    #else // psyq compiler
+    // unsigned long bssend;
+    // __asm__ volatile (
+    //      "la %0,sectend(.bss)" :"=r" (bssend)
+    //  );
+    // ...
+    #endif
+
+    size_t AvailableHeap = __stack_start - __heap_start - __stacksize;
+
+    printf("Heap start 0x%08x\n", __heap_start );
+    printf("Ram size %d\n", __ramsize);
+    printf("Available Heap %d\n", AvailableHeap);
+
+    unsigned long int* HeapAllocInit = (unsigned long int*)((unsigned long int)(__heap_start + 4) & 0xFFFFFFFC); // align to 4 byte boundary just in case
+    InitHeap3(HeapAllocInit, AvailableHeap - ((unsigned long int)__heap_start & 0x00000003) ); // remove the bytes wasted for alignment
+}
+
+void dcStackAllocator_Init(SDC_StackAllocator* allocator, size_t size)
+{
+    allocator->base = (unsigned char*)malloc3(size);
+    allocator->current = allocator->base;
+}
+
+void dcStackAllocator_Release(SDC_StackAllocator* allocator)
+{
+    free3(allocator->base);
+}
+
+void* dcStackAllocator_Alloc(SDC_StackAllocator* allocator, size_t size, size_t align)
+{
+    unsigned char* alignedAddr = ((unsigned long)allocator->current + align - 1) & ~(align - 1);
+    if( alignedAddr + size < allocator->end)
+    {
+        allocator->current = alignedAddr + size;
+        return alignedAddr;
+    }
+    return NULL;
+}
+
+void dcStackAllocator_Free(SDC_StackAllocator* allocator, void* ptr)
+{
+    if( allocator->current > (unsigned char*)ptr)
+    {
+        allocator->current = ptr;
+    }
+}
+
+void dcStackAllocator_Rewind(SDC_StackAllocator* allocator)
+{
+    allocator->current = allocator->base;
+}
---
+++ dcMemory.h
@@ -0,0 +1,37 @@
+
+/* MEMORY */
+
+#ifndef _DC_MEMORY_H
+#define _DC_MEMORY_H
+
+/* Initialize system heap memory so it can be used */
+void dcMemory_Init();
+
+
+/* Simple stack allocator */
+typedef struct SDC_StackAllocator
+{
+    unsigned char* base;
+    unsigned char* current;
+    unsigned char* end;
+
+} SDC_StackAllocator;
+
+/* Initializes an stack allocator with the specified size bytes */
+void dcStackAllocator_Init(SDC_StackAllocator* allocator, size_t size);
+
+/* Release back to the heap all the memory used by the stack allocator */
+void dcStackAllocator_Release(SDC_StackAllocator* allocator);
+
+/* Allocate size bytes from the stack allocator, with the specified alignment */
+void* dcStackAllocator_Alloc(SDC_StackAllocator* allocator, size_t size, size_t align);
+
+/* Frees memory up to ptr */
+void dcStackAllocator_Free(SDC_StackAllocator* allocator, void* ptr);
+
+/* Rewinds the allocator to start */
+void dcStackAllocator_Rewind(SDC_StackAllocator* allocator);
+
+
+
+#endif /* _DC_MEMORY_H */
