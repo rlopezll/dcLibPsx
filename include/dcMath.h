7be1373711b5c0f4e4e02903906bf0dd72f070df
Quim Romo <quimromo@gmail.com>
2022-12-24 02:32:30 +0100

    Added more math functions & macros

--- include/dcMath.h
+++ include/dcMath.h
@@ -2,11 +2,4 @@
 #ifndef _DC_MATH_H
 #define _DC_MATH_H
 
-#define DC_PI 0x7AB8 // 3.1416
-#define DC_ONE 0x1000 // 4096 
-
-#define DC_MUL(fixedA, fixedB) ( (fixedA * fixedB) >> 12 )
-#define DC_DIV(fixedA, fixedB) ( (fixedA / fixedB) << 12 )
-
-
 #endif /* _DC_MATH_H */
