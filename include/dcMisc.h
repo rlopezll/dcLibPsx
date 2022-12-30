334caf1a1a5da5f58acac6e47a1c216bdfafc462
Quim Romo <quimromo@gmail.com>
2022-12-30 17:18:13 +0100

    Render function to draw a line, and misc function to draw axis

--- include/dcMisc.h
+++ include/dcMisc.h
@@ -3,10 +3,14 @@
 #define _DC_MISC_H
 
 struct SDC_Mesh3D;
+struct SDC_Render;
+struct SDC_Camera;
 
 SDC_Mesh3D* dcMisc_generateSphereMesh(long radius, unsigned latDivs, unsigned longDivs);
 long dcMisc_noise(long x, long y, long z);
 long dcMisc_fbm(long x, long y, long z);
+
+void dcMisc_DrawAxis(SDC_Render* render, SDC_Camera* camera);
 //SDC_Mesh3D* dcMisc_generateGridMesh();
 
 
